2b5ec554987db63d9882848ae27603fb0b1abf57
nlillie17 <nicholaslillie@gmail.com>
2015-04-10 11:42:35 -0700

    Update setupPetition.php

--- setupPetition.php
+++ setupPetition.php
@@ -1,9 +1,8 @@
 <?php
 	
 	require_once "login.php";
-	$conn = new mysqli($hn,"root","");
+	$conn = new mysqli("localhost","root","");
 	
-
 	$query = "DROP DATABASE IF EXISTS petition";
  	$result = $conn->query($query);
  	if (!$result) {
@@ -14,87 +13,50 @@
 	if (!$result) {
 		echo "Connection Error";
 	}
-
 	$query = "USE petition";
 	$result = $conn->query($query);
 	if (!$result) {
 		echo "Connection Error switch";
 	}
-
 	$query = "CREATE TABLE user(
-	id int NOT NULL AUTO_INCREMENT,
+	user_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	name text NOT NULL,
 	email text NOT NULL,
 	username text NOT NULL,
 	password text NOT NULL,
-	PRIMARY KEY (id)
-	) ENGINE=InnoDB";
+	class int NOT NULL,
+	dorm text NOT NULL) ENGINE=InnoDB";
 	$result = $conn->query($query);
 	if(!$result){
 		echo "Didn't create user table successfully.";
 	}
-
-	$query = "CREATE TABLE user_petitions(
-	id int NOT NULL,
-	petitionID int NOT NULL,
-	PRIMARY KEY (id, petitionID),
-	FOREIGN KEY(id)
-		REFERENCES user(id)
-		ON DELETE CASCADE
-		ON UPDATE CASCADE
-	) ENGINE=InnoDB";
-	$result = $conn->query($query);
-	if(!$result){
-		echo "Didn't create user_petitions table successfully.";
-	}
-
 	$query = "CREATE TABLE petitions(
-	id int NOT NULL,
-	petitionText text NOT NULL,
-	FOREIGN KEY (id)
-		REFERENCES user(id)
-		ON DELETE CASCADE
-		ON UPDATE CASCADE
-	) ENGINE=InnoDB";
+	user_id int NOT NULL,
+	title text NOT NULL,
+	body text NOT NULL,
+	pet_id int NOT NULL AUTO_INCREMENT,
+	num_backers int NOT NULL,
+	time_submitted timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	PRIMARY KEY (pet_id)) ENGINE=InnoDB";
 	$result = $conn->query($query);
 	if(!$result){
-		echo "Didn't create petitions table successfully. ";
+		echo "Didn't create petitions table successfully.";
 	}
-
-	$query = "CREATE TABLE saved_petitions(
-	id int NOT NULL,
-	petitionID int NOT NULL,
-	PRIMARY KEY (id,petitionID),
-	FOREIGN KEY (id)
-		REFERENCES user(id)
-		ON DELETE CASCADE
-		ON UPDATE CASCADE
-	) ENGINE=InnoDB";
+	$query = "CREATE TABLE backed(
+	pet_id int NOT NULL,
+	backer_ids text NOT NULL) ENGINE=InnoDB";
 	$result = $conn->query($query);
 	if(!$result){
-		echo "Didn't create saved_petitions table successfully. ";
+		echo "Didn't create petitions backed successfully. ";
 	}
-
-	$query = "CREATE TABLE backed_petitions(
-	id int NOT NULL,
-	petitionID int NOT NULL,
-	PRIMARY KEY (id,petitionID),
-	FOREIGN KEY (id)
-		REFERENCES user(id)
-		ON DELETE CASCADE
-		ON UPDATE CASCADE
-	) ENGINE=InnoDB";
+	$query = "CREATE TABLE saved(
+	user_id int NOT NULL,
+	saved_ids int NOT NULL) ENGINE=InnoDB";
 	$result = $conn->query($query);
 	if(!$result){
-		echo "Didn't create backed_petitions table successfully. ";
+		echo "Didn't create saved table successfully. ";
 	}
-
 ?>
 
 <a href="homePage.php"> Click here to get going. </a>
-
-
-
-
-
 
