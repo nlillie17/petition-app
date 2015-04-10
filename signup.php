1d2391b92fbba40f66744d0c44f25105b77a6a56
nlillie17 <nicholaslillie@gmail.com>
2015-04-10 11:43:07 -0700

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,14 +1,54 @@
-<?php
-	include "functions.php";
-	@$name = $_POST['name'];
-	@$email = $_POST['email'];
-	@$usrname = $_POST['usrname'];
-	@$class = $_POST['class'];
-	@$dorm = $_POST['dorm'];
-	@$pass1 = $_POST['pass1'];
-	@$pass2 = $_POST['pass2'];
-	$result = insertUser($name,$email,$usrname,$pass1, $pass2);
-	print_r($result);
+<!DOCTYPE html>
+<head>
+<title> Welcome!</title>
+</head>
+<body>
+<?php 
+	require "functions.php";
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$usrname = $_POST['usrname'];
+	$class = $_POST['class'];
+	$dorm = $_POST['dorm'];
+	$pass1 = $_POST['pass1'];
+	$pass2 = $_POST['pass2'];
+	session_start();
+	$_SESSION['un'] = $usrname;
+	$_SESSION['pw'] = $pass1;
+	var_dump($_SESSION);
+	// echo $name . " " . $email . " " . $usrname . " " . $class . " " . $dorm . " " . $pass1 . " " . $pass2."<br>";
+	
+	//insertuser($name, $email, $usrname, $pass1, $class, $dorm, $pass2);
 ?>
+<h1> Welcome <?php echo $name?> </h1>
 
-
+<?php	
+	$conn = new mysqli("localhost", "root", "", "petition");
+    if($pass1 == $pass2) {
+    	if(checkEmail($email) && uniqueUser($usrname)) {
+    		$query = "INSERT INTO user VALUES ('NULL', '$name', '$email', '$usrname', '$pass1', '$class', '$dorm')";
+    		$result = $conn->query($query);
+    		if(!$result) {
+    			echo "User insert error";
+    			echo "<a href='homePage.php'>Home</a>";
+    			die();
+    		}
+			
+    		echo "<a href='feed.php'>Click here to get started.</a>";
+    	}
+    }
+	
+// 	function insertUser($name, $email, $username, $pass1, $class, $dorm, $pass2){
+// 	$conn = new mysqli("localhost", "root", "");
+//     if($pass1 == $pass2) {
+//     	if(checkEmail($email) && checkUsername($username)) {
+//     		$query = "INSERT INTO user VALUES (NULL, $name, $email, $username, $pass1, $class, $dorm)";
+//     		$result = $conn->query($query);
+//     		if(!$result) {
+//     			echo "User insert error";
+//     		}
+//     	}
+//     }
+//   }
+?>
+</body>
